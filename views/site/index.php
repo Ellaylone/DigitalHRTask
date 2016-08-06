3308122b76389d4e9fd01b80a4ff627eeedc82e9
ellaylone <heamik91@yandex.ru>
2016-08-07 01:35:38 +0300

    small view refactoring

--- views/site/index.php
+++ views/site/index.php
@@ -6,24 +6,22 @@ use yii\helpers\Url;
 /* @var $this yii\web\View */
 
 $this->title = 'Rates';
-Pjax::begin(['enablePushState' => false, 'enableReplaceState' => false, 'id' => 'ratesPjax']);
+Pjax::begin(['enablePushState' => false, 'enableReplaceState' => false, 'id' => 'ratesPjax', 'clientOptions' => ['maxCacheLength' => 9999]]);
 ?>
 <div class="site-index">
     <div class="body-content">
         <div class="row">
             <div class="col-lg-4">
+                <p>
                 <?php
                 if($latestRate){
-                    echo "<p>";
                     echo $latestRate->date . " - " . $latestRate->rate;
-                    echo "</p>";
-                    echo Html::a('Refresh', Url::current());
                 } else {
-                    echo "<p>";
                     echo "Нет данных";
-                    echo "</p>";
                 }
                 ?>
+                </p>
+                <?=Html::a('Refresh', Url::current());?>
             </div>
         </div>
     </div>
