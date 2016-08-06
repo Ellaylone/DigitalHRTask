cb943c38c6c2b4100df43f4887f1013d99fc31db
ellaylone <heamik91@yandex.ru>
2016-08-06 22:08:25 +0300

    use Rates model in index, display some basic data

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -6,8 +6,9 @@ use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
-use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\LoginForm;
+use app\models\Rates;
 
 class SiteController extends Controller
 {
@@ -60,7 +61,10 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $model = new Rates();
+        return $this->render('index', [
+            'latestRate' => $model->latest,
+        ]);
     }
 
     /**
