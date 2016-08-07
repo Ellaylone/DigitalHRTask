149f1388ddddcf5f7505732066b1b4d58bdb95a8
ellaylone <heamik91@yandex.ru>
2016-08-07 16:16:30 +0300

    add afterFind and beforeValidation for rates

--- models/Rates.php
+++ models/Rates.php
@@ -43,4 +43,27 @@ class Rates extends ActiveRecord
     {
         return new RatesQuery(get_called_class());
     }
+
+    public function beforeValidate(){
+        $this->rate = json_encode($this->rate);
+
+        $date = explode(' ', $this->date);
+        $date[0] = explode('-', $date[0]);
+        $date[1] = explode(':', $date[1]);
+        $uDate = new \DateTime();
+        $uDate->setDate($date[0][0], $date[0][1], $date[0][2]);
+        $uDate->setTime($date[1][0], $date[1][1], $date[1][2]);
+        $this->date = $uDate->getTimestamp();
+        return true;
+    }
+
+    public function afterFind()
+    {
+        parent::afterFind();
+
+        $date = new \DateTime();
+        $date->setTimestamp($this->date);
+        $this->date = $date->format('Y-m-d H:i:s');
+        $this->rate = json_decode($this->rate, true);
+    }
 }
